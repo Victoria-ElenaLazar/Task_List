db1d499018ac448a228e78136ad6122c1be9b760
Victoria Smaranda <victoria.elena01@yahoo.com>
2023-12-05 15:56:58 +0200

    feat: Laravel application.Task List. Created form.blade.php

---
+++ l10-task-list/resources/views/show.blade.php
@@ -0,0 +1,24 @@
+
+@extends('layouts.app')
+
+@section('title', $task->title)
+
+@section('content')
+
+<p>{{ $task->description }}</p>
+
+@if($task->long_description)
+    <p>{{ $task->long_description }}</p>
+@endif
+
+<p>{{ $task->created_at }}</p>
+<p>{{ $task->updated_at }}</p>
+
+    <div>
+        <form action="{{ route('task.destroy', ['task' => $task->id]) }}" method="post">
+            @csrf
+            @method('DELETE')
+            <button type="submit">delete</button>
+        </form>
+    </div>
+@endsection
---
+++ l10-task-list/routes/web.php
@@ -0,0 +1,71 @@
+<?php
+
+use App\Http\Requests\TaskRequest;
+use App\Models\Task;
+use Illuminate\Http\Response;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+
+Route::get('/', function () {
+    return redirect()->route('tasks.index');
+});
+
+Route::get('/tasks', function () {
+    return view('index', [
+        'tasks' => Task::latest()->get(),
+    ]);
+})->name('tasks.index');
+
+Route::view('/tasks/create', 'create')
+->name('task.create');
+
+Route::get('/tasks/{task}/edit', function (Task $task) {
+    return view('edit', [
+        'task' => $task
+    ]);
+})->name('task.edit');
+
+
+Route::get('/tasks/{task}', function (Task $task) {
+    return view('show', [
+        'task' => $task
+    ]);
+})->name('task.show');
+
+
+
+Route::post('/tasks', function (TaskRequest $request){
+
+    $task = Task::create($request->validated());
+    return redirect()->route('task.show', ['task' => $task->id])
+        ->with('success', 'Task created successfully!');
+})->name('task.store');
+
+Route::put('/tasks/{task}', function (Task $task, TaskRequest $request){
+
+    $task->update($request->validated());
+    return redirect()->route('task.show', ['task' => $task->id])
+        ->with('success', 'Task updated successfully!');
+})->name('task.update');
+
+Route::delete('/tasks/{task}', function (Task $task){
+   $task->delete();
+   return redirect()->route('tasks.index')->with('success', 'Task has been deleted successfully!');
+})->name('task.destroy');
+
+
+Route::fallback(function () {
+    return 'Still got somewhere';
+});
